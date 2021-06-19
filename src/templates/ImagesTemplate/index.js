d161502b18ecb3e2d7a8dbbe6553a553abf90472
szymonqqaz <szymonqqaz22@gmail.com>
2021-06-19 13:42:09 +0200

    Start create the Image template

--- src/templates/ImagesTemplate/index.js
+++ src/templates/ImagesTemplate/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const ImagesTemplate = () => <h1>Images page</h1>;
+import TopBarMobile from 'components/TopBarMobile';
+import TopBarDesktop from 'components/TopBarDesktop';
+import Footer from 'components/Footer';
+
+const graphqlQuary = graphql`
+  {
+    swapi {
+      galleries(orderBy: publishedAt_DESC) {
+        id
+        image {
+          url
+        }
+        title
+        desc
+      }
+    }
+  }
+`;
+
+const ImagesTemplate = () => {
+  const {
+    swapi: { galleries },
+  } = useStaticQuery(graphqlQuary);
+
+  return (
+    <>
+      <TopBarMobile />
+      <TopBarDesktop />
+      <Footer />
+    </>
+  );
+};
 
 export default ImagesTemplate;
